f2bda79f8fd88f76255af996b81a38679a25e62e
Chad Christensen <chadchristensen2@gmail.com>
2020-02-14 11:19:23 -0500

    move service methods to their own file

---
+++ features/blogpost/blogpost.service.js
@@ -0,0 +1,35 @@
+const { BlogPost } = require('../../models');
+
+function getAllBlogPosts() {
+  return BlogPost.findAll();
+}
+
+function getBlogPostById(blogPostId) {
+  return BlogPost.findByPk(blogPostId);
+}
+
+function createBlogPost(blogPost) {
+  return BlogPost.create(blogPost);
+}
+
+function updateBlogPost(blogPostId, updatedBlogPost) {
+  // TODO: test what this returns when blogPostId passed in is for a post that does not exist
+  return BlogPost.update(updatedBlogPost, {
+    where: {
+      id: blogPostId,
+    },
+  });
+}
+
+function deleteBlogPost(blogPostId) {
+  // TODO: test what this returns when blogPostId passed in is for a post that does not exist
+  return BlogPost.destroy({ where: { id: blogPostId } });
+}
+
+module.exports = {
+  getAllBlogPosts,
+  getBlogPostById,
+  createBlogPost,
+  updateBlogPost,
+  deleteBlogPost,
+};
